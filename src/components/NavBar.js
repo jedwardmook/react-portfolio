7a6f36e629fcbfc0c419c726820832f18c31298e
JedwardMook <jedwardmook@yahoo.com>
2024-02-13 17:07:24 -0600

    Add missing files

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+function NavBar({navLinks, style}) {
+
+    const linksToDisplay = Object.entries(navLinks).map(([href, label]) => {
+        return (
+            <li key={href}>
+                <a href={href}>{label}</a>
+            </li>
+        )
+    })
+
+  return (
+    <nav>
+        <ul className={style.nav}>
+            {linksToDisplay}
+        </ul>
+    </nav>
+  )
+}
+
+  NavBar.propTypes = {
+    navLinks : PropTypes.object.isRequired,
+    style: PropTypes.object
+  };
+
+
+
+export default NavBar
+
