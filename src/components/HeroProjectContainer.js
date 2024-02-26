96018e58e9520e719929b59c14216546497073ac
JedwardMook <jedwardmook@yahoo.com>
2024-02-26 14:14:21 -0600

    Hero project shaping

--- src/components/HeroProjectContainer.js
+++ src/components/HeroProjectContainer.js
@@ -1,7 +1,19 @@
 import PropTypes from 'prop-types';
 
 function HeroProjectContainer({styles, heroProject}) {
-  const {projectName, images,} = heroProject
+  // eslint-disable-next-line no-unused-vars
+  const {projectName, images, technologiesUsed, links, description} = heroProject
+
+  const tech = technologiesUsed.map((tech, index) => {
+    if (index !== technologiesUsed.length - 1)
+      return `${tech},`
+    else
+      return `${tech}`
+  })
+
+  const link = links.map((obj, index) => {
+    return <a key={index} href={obj.link}>{obj.type}</a>
+  })
 
   return (
     <div className={styles['hero-project-container-div']}>
@@ -9,8 +21,13 @@ function HeroProjectContainer({styles, heroProject}) {
         <img src={images[0].link} />
         <p>click for more images</p>
       </div>
-      <div>
-        <p>Project: {projectName}</p>
+      <div className={styles['hero-project-info-div']}>
+        <h5>Project: </h5><p>{projectName}</p>
+        <h5>Technologies: </h5><p>{tech}</p>
+        <h5>Description: </h5><p>{description}</p>
+        <div className={styles['links-div']}>
+          {link}
+        </div>
       </div>
     </div>
   )
